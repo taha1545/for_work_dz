14c4c448f2bf5e9ef4aee39f1d996188e943adae
taha mohamed mansouri <138829599+taha1545@users.noreply.github.com>
2024-03-08 15:56:31 +0100

    Add files via upload
    this is php code training

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+try {
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "acount";
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+      if($conn){
+        echo"you are connected". "<br>";
+      }
+    if (!$conn) {
+        throw new Exception("Failed to connect to MySQL: " . mysqli_connect_error());
+    }
+} catch (Exception $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+    <a href="profile.php">see profile</a><br>
+    <a  href="addgig.php">add gig </a><br>
+    <a  href="seegig.php">see gig</a><br>
+   
+</body>
+</html>
