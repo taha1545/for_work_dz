b3de88efb7ce810c8e434b9d27629fc9c68a38fd
taha mohamed mansouri <138829599+taha1545@users.noreply.github.com>
2024-03-26 00:01:46 +0100

    php code for login page from taha

--- login.php
+++ login.php
@@ -22,25 +22,25 @@
                 <p>Welcome back ! Please login to your account.</p>
                 <br>
                 
-                <form action="#">
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
                     <div class="inputBx">
                         <span>Email</span>
                         <i class='bx bxs-envelope' style='color:#0085ff'></i>
-                        <input type="email" placeholder="Enter your Email" required>
+                        <input type="email" placeholder="Enter your Email" name="email" required>
                         
                     </div>
                     <div class="inputBx">
                         <div class="input__overlay" id="input-overlay"></div>
                         <span>Password</span>
-                        <input type="password" class="input__password" placeholder="Enter your Password" id="input-pass" required>
+                        <input type="password" class="input__password" placeholder="Enter your Password" id="input-pass" name="password" required>
                         <i class='bx bxs-hide input__icon'style='color:#0085ff' id="input-icon"></i>
 
                     </div>
                     <div class="remember">
-                        <label><input type="checkbox" name="">Remember password</label>
+                        <label><input type="checkbox" name="remembre_password">Remember password</label>
                     </div>
                     <div class="inputBx">
-                        <input type="submit" value="Log in" name="" >
+                        <input type="submit" value="Log in" name="login" >
                     </div>
                     
                         <a href="#">Forgot Username Password ?</a>
@@ -56,3 +56,58 @@
     <script src="script.js"></script>
 </body>
 </html>
+<?php
+ include("database.php");
+  session_start(); 
+  if( isset($_POST["login"]) && isset($_POST["email"]) && isset($_POST["password"]) ){
+      $email=$_POST["email"];
+      $password=$_POST["password"];
+      
+
+     //
+       $valid=false;
+       $mistak=false;
+      try{
+        $sql = " SELECT * FROM `users`";
+        $res=mysqli_query($conn,$sql);
+        if(mysqli_num_rows($res)>0){
+         while ($row = mysqli_fetch_assoc($res)) {
+
+            if( $email == $row["email"] && $password == $row["password"] ){
+                   $valid = true;
+            }else if ($email == $row["email"] && $password != $row["password"]){
+                   $mistak = true;
+            }else{
+                $valid=false;
+                $mistak=false;
+            }
+         }
+        }
+    }catch(mysqli_sql_exception $e){
+        echo "There is a problem: " . $e->getMessage();
+    }
+
+
+    //
+    if( $valid && !$mistak ){
+        $_SESSION["email"]= $email;
+        header("location:home.php");
+    }else if( $mistak ){
+         echo"incorect password";
+    }else{
+          $_SESSION["email"]= $email;
+          try{
+            $sql = "INSERT INTO users (email,password) VALUES ('$email','$password')";
+             mysqli_query($conn,$sql);
+            }catch(mysqli_sql_exception){
+            echo"there is problem";
+            }
+         header("location:home.php");
+        
+    }
+
+    }
+  
+
+ mysqli_close($conn);
+?>
