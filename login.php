aa4e0358b438508da880e3f4ce4b54bbe7f8a83a
taha mohamed mansouri <138829599+taha1545@users.noreply.github.com>
2024-03-29 00:44:16 +0100

    some changes in php for login page

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +10,9 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
+
 <body>
+
     <section>
         <div class="imgBx">
             <img src="photos/brandi-redd-aJTiW00qqtI-unsplash.jpg" alt="Background Image" >
@@ -23,28 +26,33 @@
                 <br>
                 
                 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
                     <div class="inputBx">
                         <span>Email</span>
                         <i class='bx bxs-envelope' style='color:#0085ff'></i>
                         <input type="email" placeholder="Enter your Email" name="email" required>
                         
                     </div>
+
                     <div class="inputBx">
                         <div class="input__overlay" id="input-overlay"></div>
                         <span>Password</span>
                         <input type="password" class="input__password" placeholder="Enter your Password" id="input-pass" name="password" required>
                         <i class='bx bxs-hide input__icon'style='color:#0085ff' id="input-icon"></i>
-
+                        <p style="display:none" id="mistake" >incorect password !</p>
                     </div>
+
                     <div class="remember">
                         <label><input type="checkbox" name="remembre_password">Remember password</label>
                     </div>
+
                     <div class="inputBx">
                         <input type="submit" value="Log in" name="login" >
                     </div>
                     
-                        <a href="forgotpassword.php">Forgot Username Password ?</a>
+                        <a href="#">Forgot Username Password ?</a>
                 </form>
+
                 <h3 class="words">Or login with</h3>
                 <ul class="sci">     
                     <li><img src="photos/social1.png" style="width: 40px;"></li>
@@ -52,6 +60,7 @@
                 </ul>
             </div>
         </div>
+
     </section>
     <script src="script.js"></script>
 </body>
@@ -62,6 +71,7 @@
   session_start(); 
 
   if( isset($_POST["login"]) && isset($_POST["email"]) && isset($_POST["password"]) ){
+
       $email=$_POST["email"];
       $password=$_POST["password"];
 
@@ -95,9 +105,10 @@
         $_SESSION["email"]= $email;
         header("location:home.php");
     }else if( $mistak ){
-         echo"incorect password";
+       echo" <script> document.querySelector('#mistake').style.display = 'block'; </script>";
     }else{
           $_SESSION["email"]= $email;
+         
           try{
             $sql = "INSERT INTO users (email,password) VALUES ('$email','$password')";
              mysqli_query($conn,$sql);
@@ -105,7 +116,6 @@
             echo"there is problem";
             }
          header("location:home.php");
-        
     }
 
     }
