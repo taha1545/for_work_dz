84984e6407fc312b01aef8fd564a6328ae1588d4
Yasmine <141778595+GRELYASMINE@users.noreply.github.com>
2024-04-30 20:01:58 +0100

    modification

--- navbar.js
+++ navbar.js
@@ -36,3 +36,18 @@ const body = document.querySelector("body"),
        function toggleMenu(){
         subMenu.classList.toggle("open-menu");
        }
+
+
+       ///////////////////////////
+      
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+
+
